a6fa8008f375abaf0400a6f25573ed170128c58e
Ana Júlia <aanajulia505@gmail.com>
2025-01-26 16:21:22 -0300

    DeparmentDaoJDBC implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,12 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
+
+    public static DepartmentDao createDepartmentDao(){ return new DepartmentDaoJDBC(DB.getConnection()); }
 
     //nao expor da implementação, somente a interface
     public static SellerDao createSellerDao() {
